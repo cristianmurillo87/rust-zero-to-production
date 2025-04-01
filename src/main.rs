2b782e21103e83fadac29230cc62857254a1eb8f
Cristian  Murillo <cam_725@hotmail.com>
2025-04-01 11:09:48 +0200

    implemented basic health checking request

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
 
-async fn greet(request: HttpRequest) -> impl Responder {
-    let name = request.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", name)
+async fn check_health() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(check_health)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_health_checking_succeeds() {
+        let response = check_health().await;
+        assert!(response.status().is_success())
+    }
 }
