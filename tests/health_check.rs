c610b968989330ebb5a27d43784cf54d6b8174c3
Cristian  Murillo <cam_725@hotmail.com>
2025-04-01 12:12:43 +0200

    modified run by using a tcp listener

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,22 @@
-fn spawn_test_app() {
-    let server = rust_zero_to_production::run().expect("Failed to bind address");
+use std::net::TcpListener;
+
+fn spawn_test_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = rust_zero_to_production::run(listener).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
 
 #[tokio::test]
 async fn test_health_checking_succeeds() {
-    spawn_test_app();
+    let address = spawn_test_app();
+    println!("address {}", &address);
 
     let client = reqwest::Client::new();
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
